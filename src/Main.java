642baaa27157b5f3149d697a71566bca7ae82f29
Adelina <adelshnv@gmail.com>
2023-02-11 16:25:19 +0300

    bonus

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        int balance = 100;
+        int payment = 2000;
 
-        int eaters = 5; // сколько людей будут есть
+        int bonus;
+        int finalBalance;
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        if (payment <= 1000) {
+            bonus = 0;
+            finalBalance = (balance + payment + bonus);
 
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
+        } else {
+            bonus = (payment / 100);
+            finalBalance = (balance + payment + bonus);
+        }
 
+
+        System.out.println("Количество бонусов: " + bonus);
+        System.out.println("Итоговый баланс: " + finalBalance);
     }
-}
+} 
