7d7906d8ab24da0f5e4abad3b5238086ff2cfdb8
Adelina <adelshnv@gmail.com>
2023-02-13 16:04:41 +0300

    miles

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 1000;
-        int amount = 20;
-        int miles = price / amount;
-
-        System.out.println("Колличество начисленных миль:" + miles);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
